8bfc5bbc0109ebeded8a5df023ada50e3ee52ddb
rukayaj@gmail.com <rukayaj@gmail.com>
2021-06-19 17:28:04 +0200

    Tidy

--- space_invader_matrices.py
+++ space_invader_matrices.py
@@ -1,21 +1,18 @@
 import pandas as pd
 
-def get_match_tolerance_threshold(total_chars, tolerance):
-    return total_chars - (total_chars * tolerance)
-
-
-class BaseMatrix(pd.DataFrame):
+class _BaseMatrix(pd.DataFrame):
     @property
     def _constructor(self):
-        return BaseMatrix
+        return _BaseMatrix
 
     def __init__(self, data=None, data_list=None, *args, **kwargs):
+        # TODO needs an exception raised if someone tries to make an empty radar or invader
         if data_list:
             data = [list(x) for x in data_list]
-        super(BaseMatrix, self).__init__(data, *args, **kwargs)
+        super(_BaseMatrix, self).__init__(data, *args, **kwargs)
 
 
-class RadarMatrix(BaseMatrix):
+class RadarMatrix(_BaseMatrix):
     @property
     def _constructor(self):
         return RadarMatrix
@@ -32,38 +29,41 @@ class RadarMatrix(BaseMatrix):
 
     def scan(self, invader, tolerance=0):  # A tolerance of 0 = must be perfect match, 0.2 = 80% match, etc
         i_length, i_width = invader.shape
-        match_tolerance_threshold = get_match_tolerance_threshold(i_width * i_length, tolerance)
-        print(f'threshold: {match_tolerance_threshold}')
-        found = set()
-        print(f'invader: {invader}')
-        print('--------')
-        print(f'radar: {self}')
-        print('--------')
         radar = self.pad(i_length, i_width)
-        print('--------')
-        print(f'radar padded: {radar}')
-        print('--------')
         r_length, r_width = radar.shape
+        # TODO raise exception if radar is smaller than invader
+        found = set()
+        # Maybe the nested for loops here should be an iterator which yields radar_subsets, then can do e.g.:
+        # for subset in self.subsets(width, length): if invader.matched_in(subset, tolerance) ....
         for x in range(r_width - i_width + 1):
             for y in range(r_length - i_length + 1):
-                crop_radar = radar.crop(y, y + i_length, x, x + i_width)
-                print(f'{y}:{y + i_length}, {x}:{x + i_width}')
-                print(f'crop radar: {crop_radar}')
-                print('----')
-                masked_invader = invader.copy()
-                for i, row in crop_radar.iteritems():
-                    masked_invader[i] = ['*' if row[j] == '*' else x for j, x in enumerate(masked_invader[i])]
-                number_of_matching_chars = masked_invader.isin(crop_radar).sum().sum()
-                print(f'masked invader: {masked_invader}')
-                print(f'matches: {number_of_matching_chars}')
-                if number_of_matching_chars >= match_tolerance_threshold:
-                    print(f'adding  {x - i_width + 1}, {y - i_length + 1}')
+                radar_subset = radar.crop(y, y + i_length, x, x + i_width)
+                if invader.matched_in(radar_subset, tolerance):
                     found.add((x - i_width + 1, y - i_length + 1))
 
         return found
 
 
-class InvaderMatrix(BaseMatrix):
+class InvaderMatrix(_BaseMatrix):
     @property
     def _constructor(self):
         return InvaderMatrix
+
+    def area(self):
+        length, width = self.shape
+        return length  *  width
+
+    def get_tolerance_threshold(self, tolerance):
+        total_chars = self.area()
+        return total_chars - (total_chars *  tolerance)
+
+    def count_non_matches(self, masker):
+        masked_invader = self.copy()
+        for i, row in masker.iteritems():
+            masked_invader[i] = ['*' if row[j] == '*' else x for j, x in enumerate(masked_invader[i])]
+        return masked_invader.isin(masker).sum().sum()
+
+    def matched_in(self, radar_subset, tolerance):
+        number_of_matching_chars = self.count_non_matches(radar_subset)
+        threshold = self.get_tolerance_threshold(tolerance)
+        return number_of_matching_chars >= threshold
--- temp_test.py
+++ temp_test.py
@@ -1,19 +1,14 @@
 import unittest
-from temp import _im, scan, get_match_tolerance_threshold
-from space_invader_matrices import RadarMatrix
+from space_invader_matrices import RadarMatrix, InvaderMatrix
 
 def _rm(_list):
     return RadarMatrix(data_list=_list)
 
 def _im(_list):
-    return RadarMatrix(data_list=_list)
+    return InvaderMatrix(data_list=_list)
+
 
 class TestRadarScan(unittest.TestCase):
-
-    def test_get_match_tolerance_threshold_0(self):
-        res = get_match_tolerance_threshold(500, 0)
-        self.assertEqual(res, 500)
-
     def test_finds_invaders_simple(self):
         radar = _rm(['01', '00'])
         invader = _im(['01', '00'])
@@ -33,74 +28,74 @@ class TestRadarScan(unittest.TestCase):
 
     def test_finds_invader_top_left(self):
         radar = _rm(['1000',
-                                   '1000',
-                                   '0000'])
+                     '1000',
+                     '0000'])
         invader = _im(['10', '10'])
         found = radar.scan(invader)
         self.assertEqual(found, {(0, -1), (0, 0), (-1, 2)})
 
     def test_finds_invader_bottom_right(self):
         radar = _rm(['0000',
-                                   '0001',
-                                   '0001'])
+                     '0001',
+                     '0001'])
         invader = _im(['01', '01'])
         found = radar.scan(invader)
         self.assertEqual(found, {(3, -1), (2, 1), (2, 2)})
 
     def test_finds_invader_bottom_left(self):
         radar = _rm(['0000',
-                                   '1000',
-                                   '1000'])
+                     '1000',
+                     '1000'])
         invader = _im(['10', '10'])
         found = radar.scan(invader)
         self.assertEqual(found, {(-1, -1), (0, 1), (0, 2)})
 
     def test_finds_invader_middle(self):
         radar = _rm(['0000',
-                                   '0110',
-                                   '0110',
-                                   '0000'])
+                     '0110',
+                     '0110',
+                     '0000'])
         invader = _im(['11', '11'])
         found = radar.scan(invader)
         self.assertEqual(found, {(1, 1)})
 
     def test_finds_multiple_invaders(self):
         radar = _rm(['0000000',
-                                   '0110110',
-                                   '0110110',
-                                   '0000000'])
+                     '0110110',
+                     '0110110',
+                     '0000000'])
         invader = _im(['11', '11'])
         found = radar.scan(invader)
         self.assertEqual(found, {(1, 1), (4, 1)})
 
     def test_finds_overlapping_invaders(self):
         radar = _rm(['0000000',
-                                   '0111110',
-                                   '0111110',
-                                   '0000110',
-                                   '0000000'])
+                     '0111110',
+                     '0111110',
+                     '0000110',
+                     '0000000'])
         invader = _im(['11', '11'])
         found = radar.scan(invader)
         self.assertEqual(found, {(1, 1), (2, 1), (3, 1), (4, 1), (4, 2)})
 
     # With noise
 
-    def finds_invaders_with_noise_tolerance(self):
+    def test_finds_invaders_with_noise_tolerance(self):
         radar = _rm(['0000',
-                                   '0010',
-                                   '0110',
-                                   '0000'])
+                     '0010',
+                     '0110',
+                     '0000'])
         alien = _im(['11', '11'])
-        found = scan(radar, alien, 0.25)
+        found = radar.scan(alien, 0.25)
         self.assertEqual(found, {(-1, -1), (3, -1), (1, 1), (-1, 3), (3, 3)})
 
-    def excludes_invader_with_too_much_noise(self):
+    def test_excludes_invader_with_too_much_noise(self):
         radar = _rm(['0000',
-                                   '0010',
-                                   '0010',
-                                   '0000'])
+                     '0010',
+                     '0010',
+                     '0000'])
         alien = _im(['11', '11'])
-        found = scan(radar, alien, 0.25)
+        found = radar.scan(alien, 0.25)
         self.assertEqual(found, {(-1, -1), (3, -1), (-1, 3), (3, 3)})
 
 if __name__ == '__main__':
