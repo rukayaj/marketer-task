9c250a666489b77000ddf82d4e1020110d6f0a83
rukayaj@gmail.com <rukayaj@gmail.com>
2021-06-19 12:07:40 +0200

    Change tactics to having padded matrix

--- temp.py
+++ temp.py
@@ -1,47 +1,151 @@
-from scipy.spatial.distance import hamming
+import pandas as pd
 
-def hamming_str(a, b):
-    return hamming(list(a), list(b))
+def make_df(s):
+    s = s.split('\n')
+    return pd.DataFrame([list(x) for x in s])
+
+def get_match_tolerance_threshold(total_chars, tolerance):
+    return total_chars - (total_chars * tolerance)
+
+def crop(matrix, min_y, max_y, min_x, max_x):
+    matrix = matrix.iloc[min_y:max_y, min_x:max_x]
+    return matrix.reset_index(drop=True).T.reset_index(drop=True).T
+
+def scan(radar, invader, tolerance=0):  # A tolerance of 0 = must be perfect match, 0.2 = 80% match, etc
+    i_length, i_width = invader.shape
+    match_tolerance_threshold = get_match_tolerance_threshold(i_width * i_length, tolerance)
+    print(f'threshold: {match_tolerance_threshold}')
+    found = set()
+    print(f'invader: {invader}')
+    print('--------')
+    print(radar)
+    print('--------')
+    y_padding = pd.DataFrame(columns=range(i_length - 1))
+    padded = pd.concat([y_padding, radar, y_padding], axis=1, ignore_index=True)
+    x_padding = pd.DataFrame(columns=padded.columns, index=['*' for x in range(i_width - 1)])
+    padded = pd.concat([x_padding, padded, x_padding], axis=0, ignore_index=True).fillna('*')
+    r_length, r_width = padded.shape
+    for x in range(r_width - i_width + 1):
+        for y in range(r_length - i_length + 1):
+            crop_radar = crop(padded, y, y + i_length, x, x + i_width)
+            print(f'{y}:{y + i_length}, {x}:{x + i_width}')
+            print(crop_radar)
+            print('----')
+            number_of_matching_chars = invader.isin(crop_radar).sum().sum()
+            print(f'matches: {number_of_matching_chars}')
+            if number_of_matching_chars >= match_tolerance_threshold:
+                print(f'adding  {y}, {x}')
+                import pdb; pdb.set_trace()
+                found.add((x - i_width, y - i_length))
+
+    return found
+    found = set()
+
+    # bottom edge
+    for il in range(i_length - 1, 0, -1):
+        invader_crop = crop(invader, 0, il, 0, i_width)
+        print(f'invader_crop: {invader_crop}')
+        ic_length, ic_width = invader_crop.shape
+        new_t = get_match_tolerance_threshold(ic_width * ic_length, tolerance)
+        min_y  = r_length - ic_length
+        for x in range(r_width - ic_width + 1):
+            radar_crop = crop(radar, min_y, r_length, x, x + ic_width)
+            number_of_matching_chars = invader_crop.isin(radar_crop).sum().sum()
+            print(f'radar_crop: {radar_crop}')
+            import pdb; pdb.set_trace()
+            if number_of_matching_chars >= new_t:
+                found.add((x, min_y))
+    #return found
+    found = set()
+
+    # top edge
+    print(f'invader: {invader}')
+    for il in range(i_length - 1, 0, -1):
+        invader_crop = crop(invader, il, i_length, 0, i_width)
+        print(f'invader_crop: {invader_crop}')
+        ic_length, ic_width = invader_crop.shape
+        max_y = ic_length
+        new_t = get_match_tolerance_threshold(ic_width * ic_length, tolerance)
+        for x in range(r_width - ic_width + 1):
+            radar_crop = crop(radar, 0, max_y, x, x + ic_width)
+            print(f'radar_crop: {radar_crop}')
+            number_of_matching_chars = invader_crop.isin(radar_crop).sum().sum()
+            import pdb; pdb.set_trace()
+            if number_of_matching_chars >= new_t:
+                found.add((x, ic_length - i_length))
 
 
-class Invader:
-    def __init__(self, s):
-        self.body = s.split('\n')
-        self.width = len(self.body[0])
-        self.length = len(self.body)
+    print(found)
+    return found
 
 
-class Radar:
-    def __init__(self, s):
-        self.body = s.split('\n')
-        self.width = len(self.body[0])
-        self.length = len(self.body)
-        if not self.width or not self.length:
-            raise ValueError('Must have a height and width')
-        if not all(len(x) == self.width for x in self.body):
-            raise ValueError('Must be rectangular or square in shape')
+    for y in range(r_length):
+        for x in range(r_width):
+            crop_y = min(y + i_length, r_length)
+            crop_x = min(x + i_width, r_width)
+            crop_radar = radar.iloc[y:crop_y, x:crop_x]
+            crop_invader = invader.iloc[0:crop_y, 0:crop_x]
+            print(f'radar crop: [{y}:{crop_y}, {x}: {crop_x}]')
+            print(crop_radar)
+            print(f'invader crop: [{y}:{crop_y}, {x}: {crop_x}]')
+            print(crop_invader)
+            number_of_matching_chars = crop_invader.isin(crop_radar).sum().sum()
+            if number_of_matching_chars > tolerance:
+                found.add((y, x))
+            import pdb; pdb.set_trace()
 
-    def scan(self, invader, tolerance=0):
-        found = set()
+def get_number_of_matching_chars(radar, invader):
+    return invader.isin(radar).sum().sum()
 
-        for radar_y, line in enumerate(self.body):
-            if (radar_y + invader.length) > self.length:
+
+"""
+    for radar_y, line in enumerate(self.body):
+        top_overflow = invader.length - radar_y
+        if top_overflow > 0:
+            for i in range(top_overflow):
+
+
+        if radar_y < invader.length):
+        if (radar_y + invader.length) > self.length:
+            break
+        for radar_x in range(self.width):
+            if (radar_x + invader.width) > self.width:
                 break
-            for radar_x in range(self.width):
-                if (radar_x + invader.width) > self.width:
+            cumulative_diff = 0
+            for i in range(invader.length):
+                next_radar_line = self.body[radar_y + i]
+                radar_line_subset = next_radar_line[radar_x:radar_x + invader.width]
+                cumulative_diff += hamming_str(radar_line_subset, invader.body[i])
+                if cumulative_diff > tolerance:
                     break
-                cumulative_diff = 0
-                for i in range(invader.length):
-                    next_radar_line = self.body[radar_y + i]
-                    radar_line_subset = next_radar_line[radar_x:radar_x + invader.width]
-                    cumulative_diff += hamming_str(radar_line_subset, invader.body[i])
-                    if cumulative_diff > tolerance:
-                        break
-                    if i == invader.length - 1:
-                        found.add((radar_x, radar_y))
+                if i == invader.length - 1:
+                    found.add((radar_x, radar_y))
 
-        return found
+    return found
 
 
+
+def scan_top(radar, invader, tolerance=0):
+    invader_width = invader.shape[0]
+    invader_length = invader.shape[1]
+    for y in range(invader_length):
+        for x in range(invader_width):
+            radar_crop = radar[0:y + 1][0:x + 1]
+            print('radar: ')
+            print(f'[0:{y + 1}][0:{x + 1}]')
+            print(radar_crop)
+
+            invader_crop = invader[y + 1:][x + 1:]
+            print('invader: ')
+            print(f'[{y + 1}:][{x + 1}:]')
+            print(invader_crop)
+
+            temp = radar_crop.isin(invader_crop)
+            print(temp)
+
+
+        def scan_overflow_y(radar, invader, tolerance=0):
+        for radar_y, line in enumerate(self.body):
+"""
 
 
--- temp_test.py
+++ temp_test.py
@@ -1,87 +1,112 @@
 import unittest
-from temp import Radar, Invader
-
-
-class TestRadar(unittest.TestCase):
-    def test_it_does_not_allow_empty_radars(self):
-        self.assertRaises(ValueError, Radar(''))
-
-    def test_it_does_not_allow_triangular_radars(self):
-        self.assertRaises(ValueError, Radar('0\n00\n0'))
+from temp import make_df, scan, get_match_tolerance_threshold
 
 
 class TestRadarScan(unittest.TestCase):
+    @staticmethod
+    def _strify(_list):
+        return '\n'.join(_list)
+
+    def test_get_match_tolerance_threshold_0(self):
+        res = get_match_tolerance_threshold(500, 0)
+        self.assertEqual(res, 500)
+
     def test_finds_invader_same_size_as_radar(self):
-        found = Radar('01\n00').scan(Invader('01\n00'))
+        radar = make_df('01\n00')
+        invader = make_df('01\n00')
+        found = scan(radar, invader)
         self.assertEqual(found, {(0, 0)})
 
-    def test_finds_invader_in_bigger_radar(self):
-        radar = Radar('\n'.join(['0000',
-                                 '0001',
-                                 '0000']))
-        found = radar.scan(Invader('01\n00'))
+    def test_finds_invader_top_right(self):
+        radar = make_df('\n'.join(['0001',
+                                   '0000',
+                                   '0000']))
+        invader = make_df('01\n00')
+        found = scan(radar, invader)
+        self.assertEqual(found, {(2, 0)})
+
+    def test_finds_invader_top_left(self):
+        radar = make_df('\n'.join(['0100',
+                                   '0000',
+                                   '0000']))
+        invader = make_df('01\n00')
+        found = scan(radar, invader)
+        self.assertEqual(found, {(0, 0)})
+
+    def test_finds_invader_bottom_right(self):
+        radar = make_df('\n'.join(['0000',
+                                   '0001',
+                                   '0000']))
+        invader = make_df('01\n00')
+        found = scan(radar, invader)
         self.assertEqual(found, {(2, 1)})
 
+    def test_finds_invader_bottom_left(self):
+        radar = make_df('\n'.join(['0000',
+                                   '0001',
+                                   '0000']))
+        invader = make_df('01\n00')
+        found = scan(radar, invader)
+        self.assertEqual(found, {(2, 1)})
+
+    def test_finds_invader_middle(self):
+        radar = make_df('\n'.join(['0000',
+                                   '0010',
+                                   '0000',
+                                   '0000']))
+        invader = make_df('01\n00')
+        found = scan(radar, invader)
+        self.assertEqual(found, {(1, 1)})
+
     def test_finds_multiple_invaders(self):
-        radar = Radar('\n'.join(['11000',
-                                 '11011',
-                                 '11011',
-                                 '00011']))
-        found = radar.scan(Invader('\n'.join(['11', '11', '11'])))
+        radar = make_df('\n'.join(['11000',
+                                   '11011',
+                                   '11011',
+                                   '00011']))
+        invader = make_df('\n'.join(['11', '11', '11']))
+        found = scan(radar, invader)
         self.assertEqual(found, {(0, 0), (3, 1)})
 
-    # With noise
+    def test_finds_overlapping_invaders(self):
+        radar = make_df('\n'.join(['0000000',
+                                   '0111110',
+                                   '0111110',
+                                   '0000110',
+                                   '0000000']))
+        invader = make_df('\n'.join(['11', '11']))
+        found = scan(radar, invader)
+        self.assertEqual(found, {(1, 1), (2, 1), (3, 1), (4, 1), (4, 2)})
 
-    def test_finds_one_invader_with_noise_tolerance(self):
-        radar = Radar('\n'.join(['01000',
-                                 '11011',
-                                 '11011',
-                                 '00011']))
+    # Edges and corners
+
+    def test_finds_invader_over_bottom_edge(self):
+        radar = make_df('000\n000\n010')
+        invader = make_df('01\n11')
+        found = scan(radar, invader)
+        self.assertEqual(found, {(0, 2)})
+
+    def test_finds_long_invader_over_bottom_edge(self):
+        radar = make_df('000\n000\n001\n001')
+        invader = make_df('01\n01\n11')
+        found = scan(radar, invader)
+        self.assertEqual(found, {(1, 2), (1, 3)})
+
+    def test_finds_overlapping_invaders_over_bottom_edge(self):
+        radar = make_df('000\n111\n111')
+        invader = make_df('11\n11\n11')
+        found = scan(radar, invader)
+        self.assertEqual(found, {(0, 1), (1, 1), (0, 2), (1, 2)})
+
+    def test_finds_invader_over_top_edge(self):
+        radar = make_df('011\n000\n000\n000')
+        invader = make_df('01\n01\n11')
+        found = scan(radar, invader)
+        self.assertEqual(found, {(1, -2)})
+
+    def finds_multiple_invaders_over_top_edge(self):
+        radar = Radar('11\n11\n00')
         alien = Invader('\n'.join(['11', '11', '11']))
-        tolerance = (alien.width - 1) / alien.width
-        found = radar.scan(alien, tolerance)
-        self.assertEqual(found, {(0, 0), (3, 1)})
-
-    def excludes_invader_with_too_much_noise(self):
-        radar = Radar('\n'.join(['01000',
-                                 '01011',
-                                 '11011',
-                                 '00011']))
-        alien = Invader('\n'.join(['11', '11', '11']))
-        tolerance = (alien.width - 1) / alien.width
-        found = radar.scan(alien, tolerance)
-        self.assertEqual(found, {(3, 1)})
-
-    def test_finds_multiple_invaders_with_noise_tolerance(self):
-        radar = Radar('\n'.join(['01000',
-                                 '01010',
-                                 '11011',
-                                 '00011']))
-        alien = Invader('\n'.join(['11', '11', '11']))
-        tolerance = ((alien.width - 1) / alien.width) * 2
-        found = radar.scan(alien, tolerance)
-        self.assertEqual(found, {(0, 0), (3, 1)})
-
-    def test_excludes_multiple_invaders_with_noise_tolerance(self):
-        radar = Radar('\n'.join(['01000',
-                                 '01010',
-                                 '11010',
-                                 '00011']))
-        alien = Invader('\n'.join(['11', '11', '11']))
-        tolerance = (alien.width - 1) / alien.width
-        found = radar.scan(alien, tolerance)
-        self.assertEqual(found, set())
-
-    # Edges and corners - no tolerance
-
-    def finds_invaders_over_bottom_edge_of_radar(self):
-        radar = Radar('00\n01\n11')
-        invader = ['01', '11', '11']
-        self.assertEqual(radar.scan(invader), [(0, 1)])
-
-    def finds_invaders_over_top_edge_of_radar(self):
-        radar = Radar('11\n00\n00')
-        invader = ['01', '11']
+        found = radar.scan(alien)
 
     def finds_invaders_over_left_edge_of_radar(self):
         radar = Radar('00\n10\n10')
@@ -107,6 +132,50 @@ class TestRadarScan(unittest.TestCase):
         invader = ['11', '11']
         self.assertEqual(found, [(2, 1)])
 
+
+    # With noise
+
+    def finds_one_invader_with_noise_tolerance(self):
+        radar = Radar('\n'.join(['01000',
+                                 '11011',
+                                 '11011',
+                                 '00011']))
+        alien = Invader('\n'.join(['11', '11', '11']))
+        tolerance = (alien.width - 1) / alien.width
+        found = radar.scan(alien, tolerance)
+        self.assertEqual(found, {(0, 0), (3, 1)})
+
+    def excludes_invader_with_too_much_noise(self):
+        radar = Radar('\n'.join(['01000',
+                                 '01011',
+                                 '11011',
+                                 '00011']))
+        alien = Invader('\n'.join(['11', '11', '11']))
+        tolerance = (alien.width - 1) / alien.width
+        found = radar.scan(alien, tolerance)
+        self.assertEqual(found, {(3, 1)})
+
+    def finds_multiple_invaders_with_noise_tolerance(self):
+        radar = Radar('\n'.join(['01000',
+                                 '01010',
+                                 '11011',
+                                 '00011']))
+        alien = Invader('\n'.join(['11', '11', '11']))
+        tolerance = ((alien.width - 1) / alien.width) * 2
+        found = radar.scan(alien, tolerance)
+        self.assertEqual(found, {(0, 0), (3, 1)})
+
+    def excludes_multiple_invaders_with_noise_tolerance(self):
+        radar = Radar('\n'.join(['01000',
+                                 '01010',
+                                 '11010',
+                                 '00011']))
+        alien = Invader('\n'.join(['11', '11', '11']))
+        tolerance = (alien.width - 1) / alien.width
+        found = radar.scan(alien, tolerance)
+        self.assertEqual(found, set())
+
+    # Edges and corners - no tolerance
 
 if __name__ == '__main__':
     unittest.main()
